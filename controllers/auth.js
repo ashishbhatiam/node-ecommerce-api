f986e062209a1a3ca1968f9aead71fcc2bf47b26
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-11-04 22:13:44 +0530

    added basic user > register create with already exists validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,17 @@
 const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { BadRequestError } = require('../errors')
 
 const register = async (req, res) => {
-  res.send('Register User!')
+  const { name, password, email } = req.body
+  const emailAlreadyExists = await User.findOne({ email })
+  if (emailAlreadyExists) {
+    throw new BadRequestError(
+      'Email already exists, Please try a different email.'
+    )
+  }
+  const user = await User.create({ name, password, email })
+  res.status(StatusCodes.CREATED).json(user)
 }
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,30 +1,34 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const UserSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'Please provide name'],
-    maxLength: 50
-  },
-  email: {
-    type: String,
-    required: [true, 'Please provide email'],
-    validate: {
-      validator: validator.isEmail,
-      message: 'Please provide valid email'
+const UserSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide name'],
+      maxLength: 50
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide email'],
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'Please provide valid email'
+      }
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide password'],
+      minLength: 5
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user'
     }
   },
-  password: {
-    type: String,
-    required: [true, 'Please provide password'],
-    minLength: 5
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'user'],
-    default: 'user'
-  }
-})
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('User', UserSchema)
