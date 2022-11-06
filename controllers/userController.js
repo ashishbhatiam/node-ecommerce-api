2d1fdc46c8c6733009f11895ae58b910643fce13
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-11-06 21:07:12 +0530

    added Get All Users & Get Single User

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,19 @@
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const { NotFoundError } = require('../errors')
+
 const getAllUsers = async (req, res) => {
-  res.send('Get All Users!')
+  const users = await User.find({ role: 'user' }).select('-password')
+  res.status(StatusCodes.OK).json({ users, count: users.length })
 }
 
 const getSingleUser = async (req, res) => {
-  res.send(req.params)
+  const { id } = req.params
+  const user = await User.findOne({ _id: id, role: 'user' }).select('-password')
+  if (!user) {
+    throw new NotFoundError(`No user found with id: ${id}.`)
+  }
+  res.status(StatusCodes.OK).json(user)
 }
 
 const getCurrentUser = async (req, res) => {
