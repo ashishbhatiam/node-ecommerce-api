ada4d6e9ac0866144dcde4ec4603e80479a4d261
Ashish Bhatia <ashishbhatiam@gmail.com>
2022-11-26 00:48:37 +0530

    added create order, calculate prices, creating & create payment intent changes

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,9 @@
+const Product = require('../models/Product')
+const Order = require('../models/Order')
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const { BadRequestError, NotFoundError } = require('../errors')
+const { StatusCodes } = require('http-status-codes')
+
 const getAllOrders = async (req, res) => {
   res.send('Get All Orders')
 }
@@ -11,7 +17,60 @@ const getCurrentUserOrders = async (req, res) => {
 }
 
 const createOrder = async (req, res) => {
-  res.send('Create Order')
+  const { tax, shippingFees, cartItems } = req.body
+
+  if (!cartItems || cartItems?.length < 1) {
+    throw new BadRequestError('No cart items provided.')
+  }
+
+  if (!tax || !shippingFees) {
+    throw new BadRequestError('please provide tax & shipping fee')
+  }
+  let orderItems = []
+  let subtotal = 0
+  for (const item of cartItems) {
+    const dbProduct = await Product.findOne({ _id: item.product })
+    if (!dbProduct) {
+      throw new NotFoundError(`No product found with id: ${item.product} `)
+    }
+
+    const { _id, name, price, image } = dbProduct
+    const singleCartItem = {
+      name,
+      image,
+      price,
+      amount: item.amount, // Num of item/quantity
+      product: _id
+    }
+    // add item to order list
+    orderItems = [...orderItems, singleCartItem]
+    // Calculate subtotal
+    subtotal += item.amount * price
+  }
+
+  // Calculate total
+  const total = tax + shippingFees + subtotal
+
+  // Create Stripe Test Payment Intent
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'inr'
+  })
+
+  const orderPayload = {
+    tax,
+    shippingFees,
+    subtotal,
+    total,
+    orderItems,
+    user: req.user.id,
+    clientSecret: paymentIntent.client_secret,
+    paymentIntentId: paymentIntent.id
+  }
+
+  // Create Order
+  const order = await Order.create(orderPayload)
+  res.status(StatusCodes.CREATED).json(order)
 }
 
 const updateOrder = async (req, res) => {
